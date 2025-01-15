384441b61abe4c3f815011ddf70fa1942ddbd6a1
FreddyP <fredwardpach@gmail.com>
2025-01-15 13:17:53 -0500

    Added contactList to be contact homepage

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -2,3 +2,7 @@
 document.getElementById("myButton").addEventListener("click", function () {
     console.log("Button was clicked!");
 });
+document.getElementById("logOut").addEventListener("click", function () {
+    // Redirect to another file
+    window.location.href = "index.html";
+});
