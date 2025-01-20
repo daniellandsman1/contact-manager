9278885ed625f8e31310a5ec4b95d66475fe71fb
Arthur <arthurteng0819@gmail.com>
2025-01-20 16:39:44 -0500

    finished mobile ver for Login Page

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,8 +1,1 @@
 // script.js
-document.getElementById("myButton").addEventListener("click", function () {
-    console.log("Button was clicked!");
-});
-document.getElementById("logOut").addEventListener("click", function () {
-    // Redirect to another file
-    window.location.href = "index.html";
-});
