a4e7be5b0161effa60e852e3ddfa9c7787f5901c
Arthur <arthurteng0819@gmail.com>
2025-02-02 15:32:50 -0500

    added passwords do not match and first anme and last name connected with backend. login and signup is done

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,15 +1,14 @@
-
 <?php
-	header("Access-Control-Allow-Origin: *");
-	header("Access-Control-Allow-Methods: POST");
-	header("Access-Control-Allow-Headers: Content-Type");
-	header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json");
 
-	
-	if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-		header("HTTP/1.1 200 OK");
-		exit(0);
-	}
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 	$inData = getRequestInfo();
 	
 	$id = 0;
